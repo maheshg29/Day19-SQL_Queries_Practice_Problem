66398483784e27c6a5319168de66619204c1e4b4
mahesh <gangurdemahesh9@gmail.com>
2023-04-17 00:10:28 +0530

    [mahesh]add program filr for Modify Table and insert value in oit

--- Day19_SQL_Practice_Problem.sql
+++ Day19_SQL_Practice_Problem.sql
@@ -14,7 +14,20 @@ CREATE TABLE Customer (
 );
 SELECT * FROM Customer;
 
+/*3) Add few customer details into customer table.*/
 INSERT INTO Customer (CustomerName, Phone, Address) VALUES 
 ('mahesh', '7894561237', '12B shanti nagar'),
 ('akshay', '9871234565', '10D Shree Nagar'),
 ('Ganesh', '7845961235', '27A OM Nagar');
+
+/*4) Add new columns like Country, Salary and Pincode and update values.*/
+ALTER TABLE Customer ADD Country VARCHAR(50);
+ALTER TABLE Customer ADD Salary DECIMAL(10,2);
+ALTER TABLE Customer ADD Pincode VARCHAR(10);
+
+UPDATE Customer SET Country='USA', Salary=55000.00, Pincode='424001' WHERE CustomerName='mahesh';
+UPDATE Customer SET Country='Canada', Salary=66000.00, Pincode='456001' WHERE CustomerName='akshay';
+UPDATE Customer SET Country='India', Salary=44000.00, Pincode='520140' WHERE CustomerName='Ganesh';
+
+
+
