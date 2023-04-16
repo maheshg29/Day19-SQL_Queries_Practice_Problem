c6d104f30e5acd27b4cfb2f27bb2c4a28bc3ffd0
mahesh <gangurdemahesh9@gmail.com>
2023-04-16 23:52:10 +0530

    [mahesh]add program file for Creat Table in DataBase

--- Day19_SQL_Practice_Problem.sql
+++ Day19_SQL_Practice_Problem.sql
@@ -1,3 +1,15 @@
 //*1) Create Database. */
 CREATE DATABASE Day19DB;
 use Day19DB;
+
+//*2) Create Customer table with columns Customer Id, Customer Name,
+Phone, Address.
+Note: Make Customer Id as auto increment. */
+
+CREATE TABLE Customer (
+    CustomerId INT IDENTITY(1,1) PRIMARY KEY,
+    CustomerName VARCHAR(255),
+    Phone VARCHAR(20),
+    Address VARCHAR(255)
+);
+SELECT * FROM Customer;
